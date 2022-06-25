1898e370396751787c01809a4954bee3bfabec50
wangweijie <xijing9705@163.com>
2022-06-25 14:41:43 +0800

    array第一次提交

---
+++ ArrayTest/src/com/huatian/array/Demo1Array.java
@@ -0,0 +1,15 @@
+package com.huatian.array;
+
+public class Demo1Array {
+    public static void main(String[] args) {
+        /**
+         * 数组的定义格式
+         * 1.数据类型[]  数组名
+         * 2.数据类型  数组名【】
+         */
+        int[] arr = new int[5];
+        System.out.println(arr);
+        byte[] bytes = new byte[6];
+        System.out.println(bytes);
+    }
+}
